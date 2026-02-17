76599d32266a1d09c4c6a1ef5ee98f978a2badd3
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-02-17 20:59:11 +0530

    Time: 1 ms (100%), Space: 35.1 MB (69.61%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cs
+++ 0326-power-of-three/0326-power-of-three.cs
@@ -1,5 +1,10 @@
 public class Solution {
     public bool IsPowerOfThree(int n) {
-         return n > 0 && 1162261467 % n == 0;
+       if(n<=0)return false;
+       while(n%3==0){
+        n/=3;
+       }
+       return n==1;
+
     }
 }
