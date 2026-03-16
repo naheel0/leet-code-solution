977e8ccf1784c036a8f862c25416574b22dd1c24
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-03-16 21:08:23 +0530

    Time: 382 ms (97.84%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 SELECT p.firstName,p.lastName,a.city,a.state
 FROM Person p
-LEFT JOIN Address a 
-ON p.personId=a.personId
+LEFT JOIN Address a
+ON a.personId=p.personId
