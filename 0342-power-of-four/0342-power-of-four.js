1ec9d80af0c56a64d9d0f5ff8758548035b110dc
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-03-31 21:23:27 +0530

    Time: 0 ms (100%), Space: 55.5 MB (78.2%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n<=0){
+        return false;
+    }else{
+        if((n & (n - 1)) == 0 && n % 3 == 1){
+            return true
+        }else{
+            return false
+        }
+    }
+};
