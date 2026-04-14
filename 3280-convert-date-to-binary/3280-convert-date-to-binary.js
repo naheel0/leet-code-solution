43c329935bf069b808b860eb92764c90803a1fe1
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-04-14 10:17:26 +0530

    Time: 1 ms (60%), Space: 57.1 MB (5.71%) - LeetHub

---
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var convertDateToBinary = function(date) {
+    let newDate=date.split("-")
+    let year=Number(newDate[0]).toString(2);
+    let month=Number(newDate[1]).toString(2);
+    let day=Number(newDate[2]).toString(2);
+    let result=[year,month,day].join("-");
+    return result;
+};
