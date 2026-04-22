e724e6893928f0b5c0e7fcca86840863298e182d
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-04-22 10:27:19 +0530

    Time: 0 ms (100%), Space: 53.6 MB (55.52%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let product=1;
+    let sum=0;
+    let str=n.toString();
+    for(let i=0;i<str.length;i++){
+        let digit=Number(str[i])
+        product*=digit;
+        sum+=digit;
+    }
+    return product-sum;
+};
