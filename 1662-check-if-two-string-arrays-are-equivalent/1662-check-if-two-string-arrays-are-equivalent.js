550edbe7e08faccff96a557a6e3f2e39e414bd4c
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-04-23 12:42:57 +0530

    Time: 0 ms (100%), Space: 54.3 MB (17.67%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    Word1=word1.join("");
+    Word2=word2.join("");
+    return Word1==Word2;
+};
