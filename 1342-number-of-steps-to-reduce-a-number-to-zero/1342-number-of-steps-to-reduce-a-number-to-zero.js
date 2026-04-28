0344d8d0ac1035a554ab205b47b93c9c8adeef59
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-04-28 10:21:51 +0530

    Time: 0 ms (100%), Space: 53.3 MB (78.85%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let result=0;
+    while(num>0){
+        if(num%2==0){
+            num=num/2
+            result++
+        }else{
+            num=num-1
+            result++
+        }
+    }
+    return result;
+};
