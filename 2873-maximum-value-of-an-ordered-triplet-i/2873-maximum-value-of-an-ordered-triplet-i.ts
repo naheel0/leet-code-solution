73591af9d93188dbb2b4d7dcfeb70d958c54157d
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-05-07 21:44:40 +0530

    Time: 3 ms (60%), Space: 56.9 MB (100%) - LeetHub

---
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.ts
@@ -0,0 +1,12 @@
+function maximumTripletValue(nums: number[]): number {
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            for(let k=j+1;k<nums.length;k++){
+                let currentSum=(nums[i]-nums[j])*nums[k];
+                if(currentSum>sum)sum=currentSum;
+            }
+        }
+    }
+    return sum;
+};
