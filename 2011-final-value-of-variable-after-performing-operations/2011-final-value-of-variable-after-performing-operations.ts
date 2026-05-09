10c3ce5d801653758bc5c9a3e52b562ba51f0a36
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-05-09 21:46:06 +0530

    Time: 46 ms (0%), Space: 56.3 MB (54.55%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.ts
@@ -0,0 +1,11 @@
+function finalValueAfterOperations(operations: string[]): number {
+    let result=0;
+    for(let i=0;i<operations.length;i++){
+        if(operations[i]=="--X"||operations[i]=="X--"){
+            result--;
+        }else if(operations[i]=="++X"|| operations[i]=="X++"){
+            result++;
+        }
+    }
+    return result;
+};
