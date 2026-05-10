495be01b278d797b725edbba74904b92cc896688
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-05-10 15:55:10 +0530

    Time: 4 ms (61.29%), Space: 78.8 MB (74.19%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] FindArray(int[] pref) {
+        int [] arr=new int [pref.Length];
+        arr[0]=pref[0];
+        for(int i=1;i<pref.Length;i++){
+            arr[i]=(pref[i-1]^pref[i]);
+        }
+        return arr;
+    }
+}
