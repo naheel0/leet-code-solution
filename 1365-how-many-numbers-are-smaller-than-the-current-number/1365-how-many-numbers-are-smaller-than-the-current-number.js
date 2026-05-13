2bc0e9fc7546da3b88b5f8c2ba85a218940f870e
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-05-13 21:55:12 +0530

    Time: 11 ms (33.07%), Space: 56.7 MB (79.89%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -5,13 +5,14 @@
 var smallerNumbersThanCurrent = function(nums) {
     let result=[];
     for(let i=0;i<nums.length;i++){
-        let numSmall=0;
+        let count=0;
         for(let j=0;j<nums.length;j++){
-            if(nums[i]>nums[j]){
-               numSmall++
+            if(nums[i]!=nums[j]){
+                if(nums[i]>nums[j]) count++
             }
         }
-        result.push(numSmall);
+        result.push(count);
     }
     return result;
+
 };
