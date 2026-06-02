be420515aa14680268d4c2bb9bd8ea4c8e11f6d6
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-06-02 21:15:09 +0530

    Time: 4 ms (36.94%), Space: 58.1 MB (64.6%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let l=0;
+    let r=nums.length-1;
+    let res=[];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            res[l]=nums[i]
+            l++;
+        }else{
+            res[r]=nums[i]
+            r--;
+        }
+    }
+    return res;
+};
