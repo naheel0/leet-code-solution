1968d48e97ba69b16af93f997e6b040d4d070e99
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-08-14 10:00:47 +0530

    Time: 333 ms (67.25%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT MAX(num) AS num
+FROM MyNumbers
+WHERE num IN (
+    SELECT num 
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*)='1'
+) 
+ 
