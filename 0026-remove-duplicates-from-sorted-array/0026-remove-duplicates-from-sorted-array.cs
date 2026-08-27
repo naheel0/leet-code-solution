f64b38108d420d86be9ad3e541e5ea89fc2c3c4c
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-08-27 09:28:55 +0530

    Time: 8 ms (8.05%), Space: 52.2 MB (5.86%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+      int[] unique = nums.Distinct().ToArray();
+      for(int i=0;i<unique.Length;i++){
+       nums[i]=unique[i];
+  }
+  return unique.Length;
+  
+    }
+}
