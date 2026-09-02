ea2d917aa65471fe2c685a1be8124bb96c82d5cc
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-09-02 09:39:23 +0530

    Time: 56 ms (5.3%), Space: 53 MB (81.46%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function(n) {
+    let match=0;
+    while(n>1){
+        if(n%2==0){
+           match+=n / 2 
+           n=n/2 
+        }else{
+            match+=(n - 1) / 2 + 1
+            n=(n - 1) / 2
+        }
+    }
+    return match;
+};
